260656fec0af1543d597cf43d9327761e42cba74
masudur0823 <masudur0823@gmail.com>
2023-06-25 14:15:31 +0600

    e

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,11 +4,15 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Header() {
-  const pathname  = usePathname();
+  const pathname = usePathname();
   const navList = [
     {
       name: "Home",
       path: "/",
+    },
+    {
+      name: "Income",
+      path: "/income",
     },
     {
       name: "Expense",
@@ -16,30 +20,26 @@ export default function Header() {
     },
   ];
 
-  const style = {
-    inactive: "text-slate-400 py-2 px-5 border border-slate-400 rounded-lg",
-    active: "text-white py-2 px-5 border border-white rounded-lg",
-  };
   return (
-    <div className="flex justify-center p-3 bg-slate-600">
-      <div className="flex gap-2">
-        {navList.map((item, index) => (
-          <Link
-            key={index}
-            href={item.path}
-            className={pathname === item.path ? style.active : style.inactive}
-          >
-            {item.name}
-          </Link>
-        ))}
-
-        {/* <Link
-          href="/expense"
-          className={segment === "expense" ? style.active : style.inactive}
-        >
-          Examples
-        </Link> */}
-      </div>
+    <div className={style.main}>
+        {navList.map((item, index) => {
+          const isActive = pathname === item.path;
+          return (
+            <Link
+              key={index}
+              href={item.path}
+              className={isActive ? style.active : style.inactive}
+            >
+              {item.name}
+            </Link>
+          );
+        })}
     </div>
   );
 }
+
+const style = {
+  main:"flex justify-center gap-2 p-3 bg-slate-600",
+  inactive: "text-slate-400 py-2 px-5 border border-slate-400 rounded-lg",
+  active: "text-white py-2 px-5 border border-white rounded-lg",
+};
