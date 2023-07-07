23d16765b6d100db38a2ac41336760f81e3c1f1a
masudur0823 <masudur0823@gmail.com>
2023-07-07 22:23:33 +0600

    e

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+"use client";
+import { db } from "@/firebase/firebase_config";
+import useGetdata from "@/hooks/useGetdata";
+import { collection } from "firebase/firestore";
+import { useEffect } from "react";
 import { FaMoneyBillAlt } from "react-icons/fa";
 
 export default function Home() {
   const { getData, data } = useGetdata();
+  const expenseCollectionRef = collection(db, "expense");
+  // get expense
+  useEffect(() => {
+    getData(expenseCollectionRef);
+  }, []);
+
+  const totalAmount = data?.reduce((a, c) => a + c.amount, 0);
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-col md:flex-row gap-5 justify-center my-5">
@@ -14,7 +27,7 @@ export default function Home() {
         <Card
           title="Total Expense"
           bg="bg-red-600"
-          amount={400}
+          amount={totalAmount}
           icon={<FaMoneyBillAlt />}
         />
       </div>
